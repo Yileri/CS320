7425b0228b3826c0fa5e4c70dd93e0f012b25886
berratasdemir <berra.tasdemir@ozu.edu.tr>
2022-12-06 15:39:10 +0300

    Added getter and setter methods

--- LibraryManagement/src/Product.java
+++ LibraryManagement/src/Product.java
@@ -4,4 +4,36 @@ public class Product {
     private String productName;
     private int year;
     private String genre;
+
+    public int getProductID(){
+        return productID;
+    }
+    public void setProductID(){
+
+    }
+    public String getProductName(){
+        return productName;
+    }
+
+    public void setProductName(){
+
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void setYear(){
+
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public void setGenre(){
+
+    }
+
+
 }
--- LibraryManagement/src/User.java
+++ LibraryManagement/src/User.java
@@ -2,4 +2,19 @@ public class User {
 
     private int userID;
     private String userName;
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public void setUserName(){
+
+    }
+
+    public int getUserID(){
+        return userID;
+    }
+    public void setUserID(){
+
+    }
 }
