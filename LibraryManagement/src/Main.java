13e3325ea861f8cdb8054cd6d46ac4ea656e49cf
resoresoreso <resat.bulut@ozu.edu.tr>
2022-12-14 15:44:29 +0300

    implemented connection to DB and added a unit test to check the connection to DB with Ekin

--- LibraryManagement/src/Main.java
+++ LibraryManagement/src/Main.java
@@ -1,5 +1,31 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+
+        Connection conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306", "1JMibyaVTO", "6hX7WHl7uj");
+
+
+//// Create a statement
+//        Statement stmt = conn.createStatement();
+//
+//// Execute a query
+//        ResultSet rs = stmt.executeQuery("SELECT * FROM mytable");
+//
+//// Loop through the result set
+//        while (rs.next()) {
+//            // Retrieve data from a column in the current row
+//            int id = rs.getInt("id");
+//            String name = rs.getString("name");
+//
+//            // Do something with the data...
+//        }
+
+//// Close the result set, statement, and connection
+//        rs.close();
+//        stmt.close();
+
+
+            conn.close();
     }
 }
