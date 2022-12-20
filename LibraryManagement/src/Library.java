a82fc23c8525628ff7b808cbbe152c0bec2ff007
öznur timur <oznur.timur@ozu.edu.tr>
2022-12-20 19:26:14 +0300

    remove methods will be filled

--- LibraryManagement/src/Library.java
+++ LibraryManagement/src/Library.java
@@ -1,45 +1,45 @@
 import java.sql.*;
 
 public class Library {
-    private static final String DB_URL ="jdbc:mysql://remotemysql.com:3306" ;
-    private static final String PASS ="6hX7WHl7uj" ;
+    private static final String DB_URL = "jdbc:mysql://remotemysql.com:3306";
+    private static final String PASS = "6hX7WHl7uj";
     private static final String USERNAME = "1JMibyaVTO";
 
 
-        public void addBook(int productID, String productName, int year, String genre, String authorName, boolean isReserved) {
-            Connection conn = null;
-            PreparedStatement stmt = null;
+    public void addBook(int productID, String productName, int year, String genre, String authorName, boolean isReserved) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try {
+            // Establish a connection to the database
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASS);
+
+            // Create a prepared statement to insert the new book into the Books table
+            String sql = "INSERT INTO Books (productID, productName, year, genre, authorName,isReserved) VALUES (?, ?, ?, ?, ?,?)";
+            stmt = conn.prepareStatement(sql);
+
+            // Set the values for the prepared statement
+            stmt.setInt(1, productID);
+            stmt.setString(2, productName);
+            stmt.setInt(3, year);
+            stmt.setString(4, genre);
+            stmt.setString(5, authorName);
+            stmt.setBoolean(6, isReserved);
+
+            // Execute the prepared statement
+            stmt.executeUpdate();
+        } catch (SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            // Close resources
             try {
-                // Establish a connection to the database
-                conn = DriverManager.getConnection(DB_URL, USERNAME, PASS);
-
-                // Create a prepared statement to insert the new book into the Books table
-                String sql = "INSERT INTO Books (productID, productName, year, genre, authorName,isReserved) VALUES (?, ?, ?, ?, ?,?)";
-                stmt = conn.prepareStatement(sql);
-
-                // Set the values for the prepared statement
-                stmt.setInt(1, productID);
-                stmt.setString(2, productName);
-                stmt.setInt(3, year);
-                stmt.setString(4, genre);
-                stmt.setString(5, authorName);
-                stmt.setBoolean(6,isReserved);
-
-                // Execute the prepared statement
-                stmt.executeUpdate();
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
             } catch (SQLException se) {
-                // Handle errors for JDBC
                 se.printStackTrace();
-            } finally {
-                // Close resources
-                try {
-                    if (stmt != null) stmt.close();
-                    if (conn != null) conn.close();
-                } catch (SQLException se) {
-                    se.printStackTrace();
-                }
             }
         }
+    }
 
     public void addMovie(int productID, String productName, int year, String genre, String directorName, boolean isReserved) {
         Connection conn = null;
@@ -58,7 +58,7 @@ public class Library {
             stmt.setInt(3, year);
             stmt.setString(4, genre);
             stmt.setString(5, directorName);
-            stmt.setBoolean(6,isReserved);
+            stmt.setBoolean(6, isReserved);
 
             // Execute the prepared statement
             stmt.executeUpdate();
@@ -77,17 +77,13 @@ public class Library {
     }
 
 
+    public void removeBook(int productID, String productName, int year, String genre, String authorName, boolean isReserved) {
 
-
-        public void removeBook(int productID, String productName, int year, String genre, String authorName, boolean isReserved) {
-
-        }
+    }
 
     public void removeMovie(int productID, String productName, int year, String genre, String directorName, boolean isReserved) {
 
     }
-
-
 
 
     public void findBook(int productID, Connection conn) {
@@ -135,3 +131,5 @@ public class Library {
         }
     }
 }
+
+
