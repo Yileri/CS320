90a8c3d913912425ef5a29c4f0fadd56447ab94e
öznur timur <oznur.timur@ozu.edu.tr>
2022-12-21 18:24:17 +0300

    Student constructer is modified

--- LibraryManagement/src/Student.java
+++ LibraryManagement/src/Student.java
@@ -1,6 +1,7 @@
 public class Student extends User {
 
-    public Student(int userID, String userName) {
+    public Student(int userID, String userName, String userType) {
         super(userID, userName);
+        userType="student";
     }
 }
