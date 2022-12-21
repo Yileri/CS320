f66a5018c7fc21779056a5f39e17ffd772c74262
öznur timur <oznur.timur@ozu.edu.tr>
2022-12-21 18:32:39 +0300

    Movie consturctor is modified

--- LibraryManagement/src/Movie.java
+++ LibraryManagement/src/Movie.java
@@ -2,13 +2,15 @@ public class Movie extends Product{
 
     private String directorName;
 
-    public Movie(int productID, String productName, int year, String genre, String directorName,boolean isReserved) {
-        this.directorName = directorName;
+    public Movie(int productID, String productName, int year, String genre, String Name,boolean isReserved) {
+        this.directorName=Name;
 
     }
     public Movie(int productID, String productName, String borrower, String dateBorrowed, String dateDue, boolean isReserved){
 
     }
+
+
 
     public String getDirectorName() {
         return directorName;
