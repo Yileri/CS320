d2e1d967d3ffd01ea56d720a9e03d9409267d5ac
öznur timur <oznur.timur@ozu.edu.tr>
2022-12-21 16:33:09 +0300

    Staff create methods parameters are updated

--- LibraryManagement/src/Staff.java
+++ LibraryManagement/src/Staff.java
@@ -6,7 +6,7 @@ public class Staff extends User{
     Staff (int userID, String userName){
         super(userID,userName);
     }
-   public void create(ArrayList<Book> BooksList , ArrayList<Movie> MooviesList ){
+   public void create(int userID, String userName ){
 
    }
 
