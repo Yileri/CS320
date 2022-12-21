81461494880bfa3d34fae1da34a83e624633bd02
öznur timur <oznur.timur@ozu.edu.tr>
2022-12-21 16:55:09 +0300

    Create method is filled for Staff class

--- LibraryManagement/src/Staff.java
+++ LibraryManagement/src/Staff.java
@@ -1,14 +1,41 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Staff extends User{
 
+    private static final String DB_URL ="jdbc:mysql://remotemysql.com:3306" ;
+    private static final String PASS ="6hX7WHl7uj" ;
+    private static final String USERNAME = "1JMibyaVTO";
 
-    Staff (int userID, String userName){
+
+
+    Staff (int userID, String userName, String userType){
         super(userID,userName);
+        userType="staff";
     }
-   public void create(int userID, String userName ){
+    public void create(int userID, String userName, String userType) {
+        // Connect to the database
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306", USERNAME, PASS);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-   }
+        // Insert the new user into the users table
+        String sql = "INSERT INTO users (user_id, user_name, user_type) VALUES (?, ?, ?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, userID);
+            pstmt.setString(2, userName);
+            pstmt.setString(3, userType);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void update(){
 
