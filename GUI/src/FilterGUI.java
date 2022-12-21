b4957d6b0d5caedff6c4138f245856f4df783772
Yileri <yigit.ileri@ozu.edu.tr>
2022-12-21 21:57:33 +0300

    added genre combobox

--- GUI/src/FilterGUI.java
+++ GUI/src/FilterGUI.java
@@ -7,7 +7,7 @@ public class FilterGUI extends JFrame {
     public static void main(String args[]) {
         JFrame filterFrame = new JFrame("Filter");
         filterFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        filterFrame.setSize(400, 400);
+        filterFrame.setSize(250, 400);
         filterFrame.setLocation(600, 120);
         filterFrame.setLayout(new FlowLayout());
 
@@ -17,8 +17,14 @@ public class FilterGUI extends JFrame {
         JLabel authorLabel = new JLabel("Author:");
         JLabel directorLabel = new JLabel("Director:");
 
+        //dropdown menus
         String types[] = {"Select", "Book", "Movie"};
         JComboBox typeBox = new JComboBox(types);
+
+        String genres[] = {"Select", "Action", "Adventure", "Comedy", "Drama",
+                "Fantasy", "Fiction", "Historical Fiction",
+                "Horror", "Mystery", "Romance", "Sci-Fi", "Thriller"};
+        JComboBox genreBox = new JComboBox(genres);
 
         String years[] = new String[124];
         years[0] = "Select";
@@ -27,26 +33,24 @@ public class FilterGUI extends JFrame {
         }
         JComboBox yearBox = new JComboBox(years);
 
+        // panels for each dropdown menu
         JPanel typePanel = new JPanel();
         typePanel.add(typeLabel);
         typePanel.add(typeBox);
+
+        JPanel genrePanel = new JPanel();
+        genrePanel.add(genreLabel);
+        genrePanel.add(genreBox);
 
         JPanel yearPanel = new JPanel();
         yearPanel.add(yearLabel);
         yearPanel.add(yearBox);
 
+        // adding panels to main frame
         filterFrame.add(typePanel);
+        filterFrame.add(genrePanel);
         filterFrame.add(yearPanel);
 
-        //filterFrame.add(typeLabel);
-        //filterFrame.add(genreLabel);
-        //filterFrame.add(yearLabel);
-        //filterFrame.add(authorLabel);
-        //filterFrame.add(directorLabel);
-
-        //filterFrame.getContentPane().add(filterPanel);
         filterFrame.setVisible(true);
-
-
     }
 }
