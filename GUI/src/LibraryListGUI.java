7d98345413554078744c3c08b258fb63488f0a99
Yileri <yigit.ileri@ozu.edu.tr>
2022-12-23 01:04:58 +0300

    added two different tables and their respective radiobuttons

--- GUI/src/LibraryListGUI.java
+++ GUI/src/LibraryListGUI.java
@@ -5,8 +5,9 @@ public class LibraryListGUI implements ActionListener {
     public static void main(String args[]) {
         JFrame libFrame = new JFrame("ULMS");
         libFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        libFrame.setSize(700, 700);
-        libFrame.setLocation(new Point(400, 40));
+        libFrame.setSize(600, 575);
+        libFrame.setLayout(new FlowLayout());
+        libFrame.setLocationRelativeTo(null);
 
         JPanel searchPanel = new JPanel();
         JButton filterButton = new JButton("Filter");
@@ -25,10 +26,45 @@ public class LibraryListGUI implements ActionListener {
             }
         });
 
-        libFrame.getContentPane().add(BorderLayout.NORTH, searchPanel);
+        libFrame.add(searchPanel);
 
-        String[] itemColumns = {"ID", "Name", "Genre", "Year", "Author/Director", "Reserved"};
-        String[][] itemDatas = {
+        JPanel bookRadioPanel = new JPanel();
+        JLabel booksLabel = new JLabel("Books:");
+        JRadioButton bookTypeButton = new JRadioButton();
+
+        JPanel movieRadioPanel = new JPanel();
+        JLabel moviesLabel = new JLabel("Movies:");
+        JRadioButton movieTypeButton = new JRadioButton();
+
+        ButtonGroup typeGroup = new ButtonGroup();
+
+        bookRadioPanel.add(booksLabel);
+        bookRadioPanel.add(bookTypeButton);
+
+        movieRadioPanel.add(moviesLabel);
+        movieRadioPanel.add(movieTypeButton);
+
+        libFrame.add(bookRadioPanel, BorderLayout.WEST);
+        libFrame.add(movieRadioPanel, BorderLayout.WEST);
+
+        typeGroup.add(bookTypeButton);
+        typeGroup.add(movieTypeButton);
+
+        bookTypeButton.setSelected(true);
+
+        //tables
+        String[] bookColumns = {"ID", "Name", "Genre", "Year", "Author", "Reserved"};
+        String[][] bookDatas = {
+                {"1", "To Kill a Mockingbird", "Fiction", "1960", "Harper Lee", "Yes"},
+                {"2", "The Great Gatsby", "Fiction", "1925", "F. Scott Fitzgerald", "Yes"},
+                {"3", "The Catcher in the Rye", "Fiction", "1951", "J.D. Salinger", "Yes"},
+                {"4", "The Grapes of Wrath", "Fiction", "1939", "John Steinbeck", "Yes"},
+        };
+
+        JTable bookTable = new JTable(bookDatas, bookColumns);
+
+        String[] movieColumns = {"ID", "Name", "Genre", "Year", "Director", "Reserved"};
+        String[][] movieDatas = {
                 {"1", "Spider-Man", "Action", "2002", "Sam Raimi", "No"},
                 {"2", "Spider-Man 2", "Action", "2004", "Sam Raimi", "Yes"},
                 {"3", "Spider-Man 3", "Action", "2007", "Sam Raimi", "No"},
@@ -39,12 +75,35 @@ public class LibraryListGUI implements ActionListener {
                 {"8", "Spider-Man: No Way Home", "Action", "2021", "Jon Watts", "Yes"}
         };
 
-        JTable itemTable = new JTable(itemDatas, itemColumns);
-        JScrollPane scrollPane = new JScrollPane(itemTable);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        JTable movieTable = new JTable(movieDatas, movieColumns);
 
-        libFrame.add(BorderLayout.CENTER, scrollPane);
-        libFrame.setVisible(true);
+        JScrollPane bookScrollPane = new JScrollPane(bookTable);
+        bookScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        JScrollPane movieScrollPane = new JScrollPane(movieTable);
+        movieScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        libFrame.add(bookScrollPane);
+
+        // switch to book list
+        bookTypeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                libFrame.remove(movieScrollPane);
+                libFrame.add(bookScrollPane);
+                libFrame.validate();
+            }
+        });
+
+        // switch to movie list
+        movieTypeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                libFrame.remove(bookScrollPane);
+                libFrame.add(movieScrollPane);
+                libFrame.validate();
+            }
+        });
 
         JMenu reserve, filter, request, returnItem, addItem;
         JMenuItem requestBook, requestMovie;
@@ -86,7 +145,7 @@ public class LibraryListGUI implements ActionListener {
 
         libFrame.add(mbAdmin);
         libFrame.setJMenuBar(mbAdmin);
-        libFrame.setLayout(null);
+        //libFrame.setLayout(null);
         libFrame.setVisible(true);
     }
     @Override
