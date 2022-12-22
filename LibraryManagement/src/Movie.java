36d03d4fa0f6f041e3a9db5a7dd20bc90f774f5a
berratasdemir <berra.tasdemir@ozu.edu.tr>
2022-12-22 12:25:26 +0300

    improved the constructor method

--- LibraryManagement/src/Movie.java
+++ LibraryManagement/src/Movie.java
@@ -2,8 +2,8 @@ public class Movie extends Product{
 
     private String directorName;
 
-    public Movie(int productID, String productName, int year, String genre, String Name,boolean isReserved) {
-        this.directorName=Name;
+    public Movie(int productID, String productName, int year, String genre, String directorName,boolean isReserved) {
+        this.directorName= directorName;
 
     }
     public Movie(int productID, String productName, String borrower, String dateBorrowed, String dateDue, boolean isReserved){
